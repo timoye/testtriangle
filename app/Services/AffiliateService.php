557bb465828524cf1da2369817d5049b9a0311a7
Timothy Soladoye <tim.soladoye@gmail.com>
2021-12-09 15:59:51 +0100

    set dublin office

--- app/Services/AffiliateService.php
+++ app/Services/AffiliateService.php
@@ -6,6 +6,14 @@ namespace App\Services;
 class AffiliateService
 {
     public $data;
+    public $dublin_office;
+    public function setDublinOffice(){
+        $this->dublin_office=[
+            'latitude'=>'53.333780',
+            'longitude'=>'-6.253470'
+        ];
+        return $this;
+    }
     public function getData(){
         $data=file_get_contents('C:\Users\ADMIN\Downloads\testtriangle.com\affiliates.txt');
 
